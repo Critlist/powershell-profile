36f956f00801584ef87ff42f607ffe6d8921c27b
Critlist <158760167+Critlist@users.noreply.github.com>
2024-05-12 17:11:49 -0500

    exppwd

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -191,6 +191,13 @@ function pgrep($name) {
     Get-Process $name
 }
 
+function explorer {
+    param($dir)
+    if ($dir -eq 'pwd') {
+        Start-Process explorer.exe -ArgumentList (Convert-Path (Get-Location))
+    }
+}
+
 function head {
     param($Path, $n = 10)
     Get-Content $Path -Head $n
