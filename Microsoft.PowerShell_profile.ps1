7a5780c6d766ac936b29d5273e26a05266d556b7
critlist <mikejhowk64@gmail.com>
2024-05-28 00:36:59 -0500

    need a new commit

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -37,7 +37,7 @@ function Update-Profile {
         Remove-Item "$env:temp/Microsoft.PowerShell_profile.ps1" -ErrorAction SilentlyContinue
     }
 }
-Update-Profile
+# Update-Profile
 
 function Update-PowerShell {
     if (-not $global:canConnectToGitHub) {
@@ -132,33 +132,15 @@ function unzip ($file) {
     $fullFile = Get-ChildItem -Path $pwd -Filter $file | ForEach-Object { $_.FullName }
     Expand-Archive -Path $fullFile -DestinationPath $pwd
 }
-# function hb {
-#     if ($args.Length -eq 0) {
-#         Write-Error "No file path specified."
-#         return
-#     }
-    
-#     $FilePath = $args[0]
-    
-#     if (Test-Path $FilePath) {
-#         $Content = Get-Content $FilePath -Raw
-#     }
-#     else {
-#         Write-Error "File path does not exist."
-#         return
-#     }
-    
-#     $uri = "http://bin.christitus.com/documents"
-#     try {
-#         $response = Invoke-RestMethod -Uri $uri -Method Post -Body $Content -ErrorAction Stop
-#         $hasteKey = $response.key
-#         $url = "http://bin.christitus.com/$hasteKey"
-#         Write-Output $url
-#     }
-#     catch {
-#         Write-Error "Failed to upload the document. Error: $_"
-#     }
-# }
+function yy {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath $cwd
+    }
+    Remove-Item -Path $tmp
+}
 function grep($regex, $dir) {
     if ( $dir ) {
         Get-ChildItem $dir | select-string $regex
@@ -271,8 +253,9 @@ Set-PSReadLineOption -Colors @{
 }
 
 ## Final Line to set prompt
+# Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 oh-my-posh.exe init pwsh --config "https://raw.githubusercontent.com/Critlist/powershell-profile/main/easy-term.omp.json" | Invoke-Expression 
-
+Import-Module PSReadLine
 # Invoke-Expression (&starship init powershell)
 
 if (Get-Command zoxide -ErrorAction SilentlyContinue) {
